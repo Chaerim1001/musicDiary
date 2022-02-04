de5d5531be52be293de263c20cfc8f5cb9fe9afa
ParkChaerim <qkrdbsk28@naver.com>
2022-02-04 22:45:19 +0900

    feat: spotify web api accessToken 생성 추가

---
+++ src/main/java/com/prac/musicdiary/utils/CreateToken.java
@@ -0,0 +1,34 @@
+package com.prac.musicdiary.utils;
+
+
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.credentials.ClientCredentials;
+import com.wrapper.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
+import org.apache.hc.core5.http.ParseException;
+
+import java.io.IOException;
+
+public class CreateToken {
+
+    private static final String CLIENT_ID = "################";
+    private static final String CLIENT_SECRET = "####################";
+
+
+    private static final SpotifyApi spotifyApi = new SpotifyApi.Builder().setClientId(CLIENT_ID).setClientSecret(CLIENT_SECRET).build();
+
+
+    public static void accesstoken() {
+        ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials().build();
+        try {
+            final ClientCredentials clientCredentials = clientCredentialsRequest.execute();
+            // Set access token for further "spotifyApi" object usage
+            spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+
+            System.out.println("Expires in: " + clientCredentials.getExpiresIn());
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+}
